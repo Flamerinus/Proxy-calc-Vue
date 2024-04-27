1dbd2cdc62176038ca93508f4182232adc0f1fb4
Flamerinus <alexmosuyevon@yahoo.es>
2024-04-27 04:27:21 +0200

    Files upload

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import vue from "@astrojs/vue";
+import tailwind from "@astrojs/tailwind";
+import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [vue(), tailwind({
+    applyBaseStyles: false
+  }), mdx(), sitemap()]
+});
