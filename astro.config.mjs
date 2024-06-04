40fa8c3edc156e1bb40de5d90c1bf964e6499a9b
Flamerinus <alexmosuyevon@yahoo.es>
2024-06-04 04:58:44 +0200

    robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from 'astro/config';
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
+
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [vue(), tailwind({
     applyBaseStyles: false
-  }), mdx(), sitemap()],
+  }), mdx(), sitemap(), robotsTxt()],
   site: 'https://flamerinus.github.io',
-  base: '/Proxycalc',
+  base: '/Proxycalc'
 });
